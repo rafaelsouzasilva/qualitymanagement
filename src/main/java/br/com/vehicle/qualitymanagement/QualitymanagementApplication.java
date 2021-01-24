d78b2474e409c5789874d47c0d0b3a7e6bb6fe82
Rafael Souza da Silva <rafael.souza.silva@hotmail.com.br>
2021-01-24 01:03:57 -0300

    Creating project

---
+++ src/main/java/br/com/vehicle/qualitymanagement/QualitymanagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.vehicle.qualitymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualitymanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualitymanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vehicle/qualitymanagement/QualitymanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vehicle.qualitymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualitymanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
